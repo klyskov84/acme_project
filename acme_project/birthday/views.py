d4dbe8f880f3373d39c97fe1fc288d8c4141e4dd
Kirill Lyskov <klyskov84@yandex.ru>
2024-03-16 10:18:12 +0300

    порядок

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import (
+    ListView, CreateView, UpdateView, DeleteView, DetailView
+)
 from django.urls import reverse_lazy
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
@@ -9,12 +11,15 @@ from .models import Birthday
 
 class BirthdayMixin:
     model = Birthday
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
     success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayCreateView(BirthdayMixin, CreateView):
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+
+
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
     pass
 
 
@@ -27,7 +32,26 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        # Возвращаем словарь контекста.
+        return context
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    pass
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
     pass
 
 
