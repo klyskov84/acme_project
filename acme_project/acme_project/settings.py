b362270047d96b736b3989a7c31fc45edad03a2a
Kirill Lyskov <klyskov84@yandex.ru>
2024-03-17 14:02:16 +0300

    decor

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-m&$lzdzkutvrbr5vt=jpm)7#g7cken_tk%($ty+w902n7wb#=e'
+SECRET_KEY = (
+    'django-insecure-m&$lzdzkutvrbr5vt=jpm)7#g7cken_tk%($ty+w902n7wb#=e'
+    )
 
 DEBUG = True
 
